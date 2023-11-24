b79b246445d2ba2d028c4e9c57f2255aa5fe921b
froxcey <danichen204@gmail.com>
2023-11-24 14:45:44 +0800

    Fix zig0.12

--- Formula/z/zig@0.12.rb
+++ Formula/z/zig@0.12.rb
@@ -4,22 +4,24 @@ class ZigAT012 < Formula
   version "0.1710+2bffd8101"
   license "MIT"
 
+  parts = version.to_s.split(".")
+
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://ziglang.org/builds/zig-macos-aarch64-0.12.0-dev.#{version}.tar.xz"
+      url "https://ziglang.org/builds/zig-macos-aarch64-0.12.#{parts[0]}-dev.#{parts[1]}.tar.xz"
       sha256 "1f991eca383b5f82da78768f5a5a8138fc98c17c6818b1fcc342aa7eac822a5c"
     elsif Hardware::CPU.avx2?
-      url "https://ziglang.org/builds/zig-macos-x86_64-0.12.0-dev.#{version}.tar.xz"
+      url "https://ziglang.org/builds/zig-macos-x86_64-0.12.#{parts[0]}-dev.#{parts[1]}.tar.xz"
       sha256 "f427003fdf8ed819d365e018ecb40b7401b7fc935d4ef4e6db7b3d4d0f9861bf"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://ziglang.org/builds/zig-linux-aarch64-0.12.0-dev.#{version}.tar.xz"
+      url "https://ziglang.org/builds/zig-linux-aarch64-0.12.#{parts[0]}-dev.#{parts[1]}.tar.xz"
       sha256 "99bbd18fad133da335509394ced22e49bdf24ff06c191fb28eb79cbd68932849"
     elsif Hardware::CPU.avx2?
-      url "https://ziglang.org/builds/zig-linux-x86_64-0.12.0-dev.#{version}.tar.xz"
+      url "https://ziglang.org/builds/zig-linux-x86_64-0.12.#{parts[0]}-dev.#{parts[1]}.tar.xz"
       sha256 "d6053a0677959564417cd3ecd5fb90809ae8dc757f4dbfe70390ebd24b632671"
     else
       odie "Unsupported Linux architecture."

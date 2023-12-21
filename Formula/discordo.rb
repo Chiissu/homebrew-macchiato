2ecbf2d6dbd046b00eda5186644294370c8aa7e0
froxcey <danichen204@gmail.com>
2023-12-21 14:59:27 +0800

    Add discordo

---
+++ Formula/discordo.rb
@@ -0,0 +1,31 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Discordo < Formula
+  desc "A lightweight, secure, and feature-rich Discord terminal client"
+  homepage ""
+  url "https://github.com/ayn2op/discordo"
+  version "0.1.0-320ec77"
+  sha256 "bc5bf10dec8178ff20b9ed1ba8b5f8280b4fb3635a43cb6df8837f245a5d3c00"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test discordo`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end

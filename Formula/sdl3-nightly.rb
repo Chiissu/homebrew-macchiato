ba86c1e20d0db7629354f84ccfe93d2a80051235
froxcey <danichen204@gmail.com>
2024-01-23 22:28:00 +0000

    [Autoupdate]: nightly_update_23-Jan

--- Formula/sdl3-nightly.rb
+++ Formula/sdl3-nightly.rb
@@ -1,7 +1,7 @@
 class Sdl3Nightly < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  version "8d25c2d2609494ea4ce4018b8b964a706ab40b28"
+  version "a71f99c71f785e88db0b48599bb52ebe76e2b78e"
   url "https://codeload.github.com/libsdl-org/SDL/tar.gz/#{version}"
   license "Zlib"
 

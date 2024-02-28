13dc6ccf4a8f8c84157f5af649729110a919a499
Froxcey <danichen204@gmail.com>
2024-02-29 03:19:16 +0800

    Update Notabena

--- Formula/notabena.rb
+++ Formula/notabena.rb
@@ -2,7 +2,7 @@ class Notabena < Formula
   desc "Pure Rust open-source note-taking app"
   homepage "https://github.com/ThatFrogDev/notabena"
 
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/ThatFrogDev/notabena/archive/refs/tags/v#{version}.tar.gz"
   sha256 "900621a586aa5b3ff568920511401c0169175228cc7b32f03ba6f67931fadbda"
   license "GPL-3.0"

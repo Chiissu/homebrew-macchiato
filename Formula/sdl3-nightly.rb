befc76686d74f9aa3ef5cfe818f5379e4235bc05
froxcey <danichen204@gmail.com>
2024-03-02 22:24:52 +0000

    [Autoupdate]: nightly_update_2-Mar

--- Formula/sdl3-nightly.rb
+++ Formula/sdl3-nightly.rb
@@ -1,7 +1,7 @@
 class Sdl3Nightly < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  version "47be24d22543f4b528221d6e27f046a38e2a8f53"
+  version "c74f273848b0259c4902c97213f5dc0760aecce9"
   url "https://codeload.github.com/libsdl-org/SDL/tar.gz/#{version}"
   license "Zlib"
 
--- Formula/zig-nightly.rb
+++ Formula/zig-nightly.rb
@@ -1,26 +1,26 @@
 class ZigNightly < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
   homepage "https://ziglang.org/"
-  version "0.12.0-dev.3124+9e402704e"
+  version "0.12.0-dev.3126+136d7c213"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
       url "https://ziglang.org/builds/zig-macos-aarch64-#{version}.tar.xz"
-      sha256 "ac9a28039dc54725024d27145896899933ce495986e4a8f468e68b78a9f87c98"
+      sha256 "a2f3808ff95bd8fa679620ea2000cb8361ac529dd92c3e4a62ab8312b7c65963"
     elsif Hardware::CPU.avx2?
       url "https://ziglang.org/builds/zig-macos-x86_64-#{version}.tar.xz"
-      sha256 "24f8b1aaca035007d70a3f466c8ad12c67a290880a6529a3258f05769e11af23"
+      sha256 "65e8b90ed8dedadb9cfed025e57219d515cf06d4ca23bcfaf962cb591b224f5c"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://ziglang.org/builds/zig-linux-aarch64-#{version}.tar.xz"
-      sha256 "6167baf9eaf0351e20a2ea8309e3152250ed7829f4d40cf4c23c7a5b733afdcc"
+      sha256 "9f2311f9f05d2a6811d5642e2acfc93a3741e6870bac99ae191782c4874f8eab"
     elsif Hardware::CPU.avx2?
       url "https://ziglang.org/builds/zig-linux-x86_64-#{version}.tar.xz"
-      sha256 "aaf98948d9d6e9ca9a1654499d3fb0c5f2ff98a777cc92ec3c3d46384e3ce72e"
+      sha256 "d57982d4983227437621bbeb2b6f8265e1d2620a42e2f09c91ae1b2ea98e5c3d"
     else
       odie "Unsupported Linux architecture."
     end

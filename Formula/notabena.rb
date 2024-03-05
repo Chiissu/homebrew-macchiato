c6b1fdb23c24e7344021ac1d1439c06ef0cc97fd
Froxcey <danichen204@gmail.com>
2024-03-05 19:18:58 +0800

    Fix notabena hash

--- Formula/notabena.rb
+++ Formula/notabena.rb
@@ -4,7 +4,7 @@ class Notabena < Formula
 
   version "0.2.0"
   url "https://github.com/ThatFrogDev/notabena/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "900621a586aa5b3ff568920511401c0169175228cc7b32f03ba6f67931fadbda"
+  sha256 "aa545a95d1bb1b055e6847432e473e7170b1cc05e9c3711fe43ca5a43f822173"
   license "GPL-3.0"
 
   head "https://github.com/ThatFrogDev/notabena.git", branch: "dev"

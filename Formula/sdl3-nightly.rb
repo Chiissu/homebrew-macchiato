4077a7e8a54ae76c2e3e65b9813b91d2f7e9aed5
froxcey <danichen204@gmail.com>
2024-05-03 22:26:37 +0000

    [Autoupdate]: nightly_update_3-May

--- Formula/sdl3-nightly.rb
+++ Formula/sdl3-nightly.rb
@@ -1,7 +1,7 @@
 class Sdl3Nightly < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  version "05c5b3398f73195e22c6f01f90bf6fc223e35561"
+  version "7856c8fb8fec5114af54299a8cafa81db507a0b6"
   url "https://codeload.github.com/libsdl-org/SDL/tar.gz/#{version}"
   license "Zlib"
 
--- Formula/sdl3_image-nightly.rb
+++ Formula/sdl3_image-nightly.rb
@@ -1,7 +1,7 @@
 class Sdl3ImageNightly < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  version "2846ddc20b82a886322cdd0c4f84db4fa7ca1011"
+  version "b3c4c720cc65dfb3414752a79479dfe0db19eb0e"
   url "https://codeload.github.com/libsdl-org/SDL_image/tar.gz/#{version}"
   license "Zlib"
 

f5c784220677f7375197effbed46dc111394dddd
Froxcey <danichen204@gmail.com>
2024-08-22 00:58:22 +0800

    Update discordo

--- Formula/discordo.rb
+++ Formula/discordo.rb
@@ -1,23 +1,23 @@
 class Discordo < Formula
   desc "A lightweight, secure, and feature-rich Discord terminal client"
   homepage "https://github.com/ayn2op/discordo"
-  version "0.1.0-a0ec15d"
+  version "0.1.0-c5a65d0"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://nightly.link/ayn2op/discordo/actions/runs/10450399243/discordo_macOS_ARM64.zip"
-      sha256 "bff84c1b6fa998bc61864a7893dd57d874d737f671eba46163ffee206f9c60c6"
+      url "https://nightly.link/ayn2op/discordo/actions/runs/10476852338/discordo_macOS_ARM64.zip"
+      sha256 "3658b6f7ba1c363bc3e9493460f449e79dd1757442a45da8ec16b11cb960d298"
     elsif Hardware::CPU.avx2?
-      url "https://nightly.link/ayn2op/discordo/actions/runs/10450399243/discordo_macOS_X64.zip"
-      sha256 "20f688adf698758accdb7b62af9a63a942e93d1181f7c0a7a090321a6e910817"
+      url "https://nightly.link/ayn2op/discordo/actions/runs/10476852338/discordo_macOS_X64.zip"
+      sha256 "9b5330eeeb0387faf1d45dc7efb8c48a8b8c50ec6af54d34e577b463c119c581"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
     if Hardware::CPU.avx2?
-      url "https://nightly.link/ayn2op/discordo/actions/runs/10450399243/discordo_Linux_X64.zip"
-      sha256 "80a438a6ae91673af43c551be2b531554f776acd1692c82a56c223f5666d7f27"
+      url "https://nightly.link/ayn2op/discordo/actions/runs/10476852338/discordo_Linux_X64.zip"
+      sha256 "7bcfd1a9e3dd8daaf4568e27efb14451c650dc01f0e9d65b0f98ce883d4b46df"
     else
       odie "Unsupported Linux architecture."
     end

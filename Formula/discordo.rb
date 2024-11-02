bb698e190dce7fe2e6311b8b1035395bb58b63b9
froxcey <danichen204@gmail.com>
2024-11-02 22:31:47 +0000

    [Autoupdate]: nightly_update_2-Nov

--- Formula/discordo.rb
+++ Formula/discordo.rb
@@ -1,23 +1,23 @@
 class Discordo < Formula
   desc "A lightweight, secure, and feature-rich Discord terminal client"
   homepage "https://github.com/ayn2op/discordo"
-  version "0.1.0-26b77d9"
+  version "0.1.0-441362f"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2050488389.zip"
-      sha256 "c457507fd5fa0af9eea02896e3bd27695d966b947f48e67d187626562f51b326"
+      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2135471458.zip"
+      sha256 "62313a00216c3923a4d59a57b49132f7f4bbc3b4be3a986630694c47e28b5ef3"
     elsif Hardware::CPU.avx2?
-      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2050488520.zip"
-      sha256 "1c34432f1a12a7a896c228dde428b0de1d1e7b180d86b0988fe692dc9fed529f"
+      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2135471788.zip"
+      sha256 "f203af4e150805db76bd222db1b804bda60521c837223c11ebbb040ff96d1995"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
     if Hardware::CPU.avx2?
-      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2050488510.zip"
-      sha256 "f9a1358de2b32e9400bc368c6b06737b9165bc71c3b6921332f2ead37ec5974e"
+      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2135471346.zip"
+      sha256 "5ca0800862cb61407199a98b6bec99a86c894c3a807c602b19a488610c1c5b2a"
     else
       odie "Unsupported Linux architecture."
     end
--- Formula/v2d.rb
+++ Formula/v2d.rb
@@ -2,9 +2,9 @@ class V2d < Formula
   desc "2D-based VTubing studio"
   homepage "https://github.com/Chiissu/v2d"
 
-  version "0.2.0"
+  version "0.2.1"
   url "https://github.com/Chiissu/v2d/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "a1b9a639a3ada002595eff8a4fa6e8e4063d62bccf0be4e6c0e07ce9cbeb4d05"
+  sha256 "36aa43b7ca8396433e5179310cbe4a5880b8ce290bb79f7e50d16f07aab7f8bd"
 
   head "https://github.com/Chiissu/v2d.git", branch: "main"
 

1b76c4c622c6e4c2d7a9f723919830f429395c4d
froxcey <danichen204@gmail.com>
2024-12-26 20:35:01 +0800

    Update jetzig

--- Formula/jetzig.rb
+++ Formula/jetzig.rb
@@ -2,21 +2,21 @@ class Jetzig < Formula
   desc "A web framework written in Zig"
   homepage "https://www.jetzig.dev"
   license "MIT"
-  version "be8e517"
+  version "0.1.0-5ebb60d"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
       url "https://www.jetzig.dev/build-macos-aarch64.zip"
-      sha256 "3f3ab4e30a947a78b81d54f7f7df9216ed7ea5637efc376a77af16f89056fd78"
+      sha256 "6c61a3d1c88b4d6dcd6e610950baec4e6fb682b200faa38b7dd26cd712b2c03e"
     elsif Hardware::CPU.avx2?
       url "https://www.jetzig.dev/build-macos-x86.zip"
-      sha256 "5230b10e8fda6ddccb955bdd2475ae5b652df8210eabb6fea529aac1234d9608"
+      sha256 "fb1e93451827b51db702aa485c8857a2ebfc6ec58201162eaff75fb9f1379165"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
     url "https://www.jetzig.dev/build-linux.zip"
-    sha256 "b0aabc50481dce29eb9fc0a71f804e4a1fd16378d3f712c57bb0e4fe28b695df"
+    sha256 "60a551035d5100d234e7f45178f46c3feef4674c8489d4c9809d5e8e67632662"
   end
 
   def install

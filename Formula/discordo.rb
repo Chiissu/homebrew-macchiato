6ad555a0096f17dbbbdb3d6b76912cc6401cbe89
froxcey <danichen204@gmail.com>
2025-04-01 22:33:31 +0000

    [Autoupdate]: nightly_update_1-Apr

--- Formula/discordo.rb
+++ Formula/discordo.rb
@@ -1,23 +1,23 @@
 class Discordo < Formula
   desc "A lightweight, secure, and feature-rich Discord terminal client"
   homepage "https://github.com/ayn2op/discordo"
-  version "0.1.0-1628535"
+  version "0.1.0-9e95b18"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2800301918.zip"
-      sha256 "5140f8e5a43f1c18f07f1a46f252792560a721247f4ab3b3bbcbd124a160f36d"
+      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2848499014.zip"
+      sha256 "ddbe7ce18db17f6b26cdde57cf81b08df245c3b703fea87032d559839a10773e"
     elsif Hardware::CPU.avx2?
-      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2800302395.zip"
-      sha256 "7a6963947f0791cc42382fd0da8d646558a98e12de5f1287fd4d2551d49167b8"
+      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2848499572.zip"
+      sha256 "16a6a03b753e7793faad692fa1994e715b957f5217f2039ad1a83fdd290a8bd3"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
     if Hardware::CPU.avx2?
-      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2800302181.zip"
-      sha256 "53eadc71dce98a3f8c23eba583ba8a4c75518a65e5272125b8066c32c77d9646"
+      url "https://nightly.link/ayn2op/discordo/actions/artifacts/2848498963.zip"
+      sha256 "8db913ca3f492268f437366b69c569aa6cd5cb2d884ac07275fba9a599cd5e39"
     else
       odie "Unsupported Linux architecture."
     end
--- Formula/zig-nightly.rb
+++ Formula/zig-nightly.rb
@@ -1,26 +1,26 @@
 class ZigNightly < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
   homepage "https://ziglang.org/"
-  version "0.15.0-dev.78+9c9d3931d"
+  version "0.15.0-dev.168+b636d56d6"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
       url "https://ziglang.org/builds/zig-macos-aarch64-#{version}.tar.xz"
-      sha256 "6a6cf6b793af38cfeeab66fb74b4fb7cccb25565a9f541a61bbb4926c2b153fd"
+      sha256 "847559c797c3f98096cc48409080ef5c98518005e151b9707f90b96a2c6732ad"
     elsif Hardware::CPU.avx2?
       url "https://ziglang.org/builds/zig-macos-x86_64-#{version}.tar.xz"
-      sha256 "6590f2bd6f815129b3ef7ff13413ec9372f12ff5e6ea81b69e59332b3d521065"
+      sha256 "8d78678fcd0f62d5cac5df5a3eedea19a56ded13a9698aaf8bb7d00b649928d7"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://ziglang.org/builds/zig-linux-aarch64-#{version}.tar.xz"
-      sha256 "524cfd040f2230df569200b723329acabd49e94085320b913d09e414939ed233"
+      sha256 "6b5b41e6b08dc23391746b920638dc71ac4958c94131f0782d71ce4a1bf5e093"
     elsif Hardware::CPU.avx2?
       url "https://ziglang.org/builds/zig-linux-x86_64-#{version}.tar.xz"
-      sha256 "8f3d6072356406d30478566764285d509a3de163f1306946c173f012f0647316"
+      sha256 "840e8437687277b4cefc67bd1a7f184c407153eabefca93fe378b9ab3d54f58d"
     else
       odie "Unsupported Linux architecture."
     end

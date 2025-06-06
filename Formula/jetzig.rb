d5bdf37e932a942f81ee4bd7074f9d2e02be6c50
froxcey <d@frox.tw>
2025-06-06 15:19:05 +0100

    update jetzig

--- Formula/jetzig.rb
+++ Formula/jetzig.rb
@@ -2,21 +2,23 @@ class Jetzig < Formula
   desc "A web framework written in Zig"
   homepage "https://www.jetzig.dev"
   license "MIT"
-  version "0.1.0-5ebb60d"
+  version "0.1.0-1cb27ff"
+
+  depends_on "zig-nightly"
 
   if OS.mac?
     if Hardware::CPU.arm? || Hardware::CPU.in_rosetta2?
-      url "https://www.jetzig.dev/build-macos-aarch64.zip"
-      sha256 "6c61a3d1c88b4d6dcd6e610950baec4e6fb682b200faa38b7dd26cd712b2c03e"
+      url "https://jetzig.dev/downloads/build-macos-aarch64.zip"
+      sha256 "80ee5548d56cd6f46b2a225168ec57092a9f54d0e7169b56b0a110f1ac96a98c"
     elsif Hardware::CPU.avx2?
-      url "https://www.jetzig.dev/build-macos-x86.zip"
-      sha256 "fb1e93451827b51db702aa485c8857a2ebfc6ec58201162eaff75fb9f1379165"
+      url "https://jetzig.dev/downloads/build-macos-x86.zip"
+      sha256 "dacf8662ded34294916acf19c605c4bf6b0fb689131307ca72a9cbbb6cf89841"
     else
       odie "Unsupported MacOS architecture."
     end
   elsif OS.linux?
-    url "https://www.jetzig.dev/build-linux.zip"
-    sha256 "60a551035d5100d234e7f45178f46c3feef4674c8489d4c9809d5e8e67632662"
+    url "https://jetzig.dev/downloads/build-linux.zip"
+    sha256 "211b53fc3da6c79e7720a09b6a459b7d56cc4d8484517f0eb92a216b4a455796"
   end
 
   def install
